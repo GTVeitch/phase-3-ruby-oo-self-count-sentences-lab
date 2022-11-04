67d589eefd3ad82afaf3c5495d1fd1ee4e721654
Gavin Veitch <gavintveitch@gmail.com>
2022-11-04 12:19:19 -0600

    commit tree 7fa3f6ccace994156b09be0212a9b94a4224aa27

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,37 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    @exa = 0
+    @sent = 0
+    @que = 0
+    @cheat = 0
+    if(self.include?("! "))
+      broken = self.split("! ")
+      @exa = broken.count
+    end
+    if(self.include?(". "))
+      broken = self.split(". ")
+      @sent = broken.count
+    end
+    if(self.include?("? "))
+      broken = self.split("? ")
+      @que = broken.count
+    end
+    if(self.include?("..."))
+      @cheat = -2
+    end
+    (@exa + @sent + @que + @cheat)
   end
 end
